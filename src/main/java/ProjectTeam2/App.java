c1aebb6c230e4f7d3d764081c852247fb93f00e6
aungyekhant <40685544@live.napier.ac.uk>
2024-10-03 14:51:29 +0630

    City Reports (Continents,Regions,CountryName,District) by Aung Ye Khant

--- src/main/java/ProjectTeam2/App.java
+++ src/main/java/ProjectTeam2/App.java
@@ -1,4 +1,5 @@
 package ProjectTeam2;
+import Reports.CityReports;
 import Reports.CountryReports;
 import DataBaseConnect.Connection;
 
@@ -8,45 +9,49 @@ import java.util.ArrayList;
 public class App
 {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws SQLException {
 
         Connection ct = new Connection();
         ct.connect();
 
         //Creating the CountyReport Object to get the Data for displaying the system.
         CountryReports cr = new CountryReports(ct.getConnection());
+        CityReports cy = new CityReports(ct.getConnection());
+//
+//        //1# Query about retrieving  and displaying the data of the population of the Country according to the Descending Order
+//        cr.displayingAboutDescendingPopulation();
+//        System.out.println("");
+//        //2# Report About the data of population of the Country order by descending according to Continent
+//        cr.displayingAboutDescendingPopulationByContinent();
+//        System.out.println("");
+//        //3# Report About the data of population of the Country order by descending according to Region
+//        cr.displayingAboutDescendingPopulationByRegion();
+//        System.out.println("");
+//        //4# Report about top populate country limit by user
+//        int N = 10;
+//        cr.displayTopPouplateCountrybyUser(N);
+//        System.out.println("");
+//
+//        //5# Report about top populate country according to Continent limit by user
+//        cr.displayTopPopulatedCountryAccordingtoContinentByUserInput(N);
+//        System.out.println("");
+//
+//        //6# Report about top populate country according to Region limit by user
+//        cr.displayTopPopulatedCountryAccordingtoRegionByUserInput(N);
+//        System.out.println("");
+        //7# (Cities in the world organised by largest population to smallest.)
+//        cy.displayThePopulationOfCity();
 
-        //1# Query about retrieving  and displaying the data of the population of the Country according to the Descending Order
-        cr.displayingAboutDescendingPopulation();
-        System.out.println("");
-        //2# Report About the data of population of the Country order by descending according to Continent
-        cr.displayingAboutDescendingPopulationByContinent();
-        System.out.println("");
-        //3# Report About the data of population of the Country order by descending according to Region
-        cr.displayingAboutDescendingPopulationByRegion();
-        System.out.println("");
-        //4# Report about top populate country limit by user
-        int N = 10;
-        cr.displayTopPouplateCountrybyUser(N);
-        System.out.println("");
+        //8#(Cities in a continent organised by largest population to smallest.)
+//        cy.getPopulationOftheCitybyContinent();
 
-        //5# Report about top populate country according to Continent limit by user
-        cr.displayTopPopulatedCountryAccordingtoContinentByUserInput(N);
-        System.out.println("");
+        //9# (Cities in a region organised by largest population to smallest.)
+//        cy.getPopulationOftheCitybyRegion();
+        //10# (cities in a country organised by largest population to smallest)
+        cy.getPopulationOftheCitybyCountry();
+        //11# (Cities in a district organised by largest population to smallest.)
+        cy.getPopulationOfthecitybyDistrict();
 
-        //6# Report about top populate country according to Region limit by user
-        cr.displayTopPopulatedCountryAccordingtoRegionByUserInput(N);
-        System.out.println("");
-
-
-        //7#
-
-        //8#
-
-        //9#
-
-        //10#
 
         ct.disconnect();
 
